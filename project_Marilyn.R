f6762249db5a88d93b7ebeb1787bde1fba9c6ad1
EMarilyn <marilyn.ello@gmail.com>
2021-05-08 03:06:12 +0200

    .

--- project_Marilyn.R
+++ project_Marilyn.R
@@ -1,7 +1,18 @@
 data0 <- read.csv("pollution.csv", h=T)
-data <- drop_na(data0) #or na.omit()
+data <- na.omit(data0)
+sapply(data, class)
+# poisson with no transform : Shiva
+# autocorr matrix gee : Olivier
+
+data$weekday <-  as.factor(data$weekday)
+data$month <-  as.factor(data$month)
+data$year <-  as.factor(data$year)
 sapply(data, class)
 
-# poisson with no transform : Shiva
-# autosorr matrix gee : Olivier
-jhdgdkjc
+
+
+summaryBy(resp~weekday, data=pollu_na,
+          FUN = function(x) { c(Number_of_units=length(x),  Mean= mean(x), standard_deviation = sd(x)) })
+
+summaryBy(resp~month, data=pollu_na,
+          FUN = function(x) { c(Number_of_units=length(x),  Mean= mean(x), standard_deviation = sd(x)) })
