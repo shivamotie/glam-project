459af868eac6fe268841e094775639c23b523dc5
EMarilyn <marilyn.ello@gmail.com>
2021-06-05 16:13:30 +0200

    final script

--- project_Marilyn.R
+++ project_Marilyn.R
@@ -2,6 +2,58 @@ data0 <- read.csv("pollution.csv", h=T)
 data <- na.omit(data0)
 sapply(data, class)
 library(gee)
+install.packages("mgcv")
+require(mgcv)
+attach(data)
+data$weekday <- as.factor(data$weekday)
+
+
+boxplot(resp , main = "resp" , col = "gray")
+hist(resp)
+
+pairs(data[1:7])
+cor(rsp,no2) #0.7869
+cor(so2,no2) #0.4542
+boxplot(resp~weekday, main="each weekday", xlab="weekdays",ylab="resp")
+boxplot(resp~month, main="each month", xlab="month", ylab="resp")
+boxplot(resp~day, main="each days", xlab="day",ylab="resp")
+boxplot(resp~year,main="each year",xlab="year",ylab="resp")
+
+plot(resp)
+plot(no2)
+plot(so2)
+plot(rsp)
+plot(o3)
+plot(temp)
+plot(hum)
+
+m1<- gam(resp~ s(no2) + s(so2) + s(rsp) + s(o3) + s(temp) + s(hum) + 
+           factor(year)+ s(day)+ factor(month), family = poisson)
+summary(m1)
+
+#try without rsp since it's not significant and we have a quite high 
+#cor between rsp and no2 (nothing crazy but still)
+m2<- gam(resp~ s(no2) + s(so2) + s(o3) + s(temp) + s(hum) + factor(year)+ 
+           s(day)+ factor(month), family = poisson)
+summary(m2)
+#nothing crazy so stick with m1
+
+#What if we add the weekday ?
+m3<- gam(resp~ s(no2) + s(so2) + s(rsp) + s(o3) + s(temp) + s(hum) + factor(year)+ s(day)+ s(month, bc="cc", k=12)+ factor(weekday), family = poisson)
+summary(m3)
+
+
+
+extractAIC(m1)
+extractAIC(m2) #little bit lower
+
+
+
+
+
+
+
+#==================================================================================================================================================================================================================================================================================================================================================
 # poisson with no transform : Shiva
 # autocorr matrix gee : Olivier
 
